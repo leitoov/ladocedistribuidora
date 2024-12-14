b3a3fa0b4b3767d7d3ec70b25de8f33329c064b8
lvincent <leonelexequielvincentgmail.com>
2024-12-14 09:53:32 -0300

    ajustes visuales en dashboard_vendedor

--- dashboard_vendedor.php
+++ dashboard_vendedor.php
@@ -283,6 +283,23 @@ $userId = $tokenData->user_id;
             </div>
         </div>
     </div>
+    <!-- Modal para mostrar mensajes -->
+    <div class="modal fade" id="modalMensaje" tabindex="-1" aria-labelledby="modalMensajeLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalMensajeLabel">Mensaje</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" id="modalMensajeCuerpo">
+                    <!-- El mensaje se llenará dinámicamente -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
